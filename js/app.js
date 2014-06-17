256c5144934c5f73e0b7dd2be4bf6b8e1241956d
Chris Cantey <cantey0701@gmail.com>
2014-06-17 12:18:40 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
 
+  $('[data-toggle=offcanvas]').click(function() {
+    $('.row-offcanvas').toggleClass('active');
+  });
+$('#sidebar .nav>li').click(function(e){
+    $('.highlight').removeClass('highlight');
+$(this).addClass('highlight');
+
+});
 $('.overlayLayer').click(function(e) {
   $('.overlayLayer').removeClass('active');
   var $this = $(this);
@@ -10,10 +18,11 @@ $('.overlayLayer').click(function(e) {
 });
 
 $(".overlayLayer").click(function () {   
+	//var inputs = dojo.query(".overlayLayer");
 	var inputs = dojo.query(".overlayLayer");
-	var visible = [-1]; 
+	var visible = [-1]; //street and city labels
 	for (var i = 0, il = inputs.length; i < il; i++) {
-		var layerLabel = $(inputs[i]).text();  
+		var layerLabel = $(inputs[i]).text();  //To select labels to check, we must nest <label tag> in htm. and somehow select that nested label
 		//console.log('layerLabel: ',layerLabel);
 		var layerCheckState = $(inputs[i]);
 		//console.log('layerCheckState: ',layerCheckState);
@@ -26,17 +35,30 @@ $(".overlayLayer").click(function () {
 		else {
 			$(layerCheckState).removeClass("active");
 			overlayLayer.setVisibleLayers([-1])
+			//var elem = $('li.current_sub').prevAll("checked:first");
+			//elem.removeClass("checked");
+			
 		}
 	}
+
 	overlayLayer.setVisibleLayers(visible);
+	legend.refresh();
+	//$('.scrollable').nanoScroller();
+
 });
 
  function objectFindByKey(array, key, value) {
+        //alert(value);
         for (var i = 0; i < array.length; i++) {
+		    //alert(array[i].name);
             if (array[i][key] === value) {
                 return array[i];
             }
         }
         return null;
     }	
+
+
+	
+
 });
