acc72592882f709dbc0e2572493870ecf9dca6e6
Chris Cantey <cantey0701@gmail.com>
2014-06-19 16:15:48 -0500

    Fix navigator.geolocation.clearWatch() method

--- js/app.js
+++ js/app.js
@@ -1,8 +1,73 @@
 $(document).ready(function () {
 
+
+
+$('#gps').click(function () {
+    map.graphics.clear();
+	if (navigator.geolocation) {
+		//showPageLoadingMsg();
+		//if you want to track as the user moves setup navigator.geolocation.watchPostion
+		// domUtils.show(("loading"));
+		$('#loading').show;
+		navigator.geolocation.getCurrentPosition(zoomToLocation, locationError);
+		gpsid = navigator.geolocation.watchPosition(showLocation, locationError);
+	}
+
+});
+
+function locationError(error) {
+  require(["esri/domUtils"], function(domUtils){
+	switch (error.code) {
+	case error.PERMISSION_DENIED:
+		alert("Location not provided");
+		navigator.geolocation.clearWatch(gpsid);
+		domUtils.hide(loading);
+		break;
+	case error.POSITION_UNAVAILABLE:
+		alert("Current location not available");
+		navigator.geolocation.clearWatch(gpsid);
+		domUtils.hide(loading);
+		break;
+	case error.TIMEOUT:
+		alert("Timeout");
+		navigator.geolocation.clearWatch(gpsid);
+		domUtils.hide(loading);
+		break;
+	default:
+		alert("unknown error");
+		navigator.geolocation.clearWatch(gpsid);
+		domUtils.hide(loading);
+		break;
+	}
+  });	
+}
+
+		  
+
+function zoomToLocation(position) {
+  require(["esri/geometry/webMercatorUtils", "esri/symbols/PictureMarkerSymbol","esri/geometry/Point","esri/graphic","esri/domUtils"], function(webMercatorUtils, PictureMarkerSymbol,Point,Graphic,domUtils){
+	//$.mobile.hidePageLoadingMsg(); //true hides the dialog
+	var pt = webMercatorUtils.geographicToWebMercator(new Point(position.coords.longitude, position.coords.latitude));
+	map.centerAndZoom(pt, 20);
+	//uncomment to add a graphic at the current location
+	var symbol = new PictureMarkerSymbol("images/bluedot.png", 40, 40);
+	map.graphics.add(new Graphic(pt, symbol));
+	domUtils.hide(loading);
+  });
+}
+
+function showLocation(location) {
+   if (location.coords.accuracy <= 500) {
+     // the reading is accurate, do something
+   } else {
+     // reading is not accurate enough, do something else
+   }
+}
+				
   $('[data-toggle=offcanvas]').click(function() {
     $('.row-offcanvas').toggleClass('active');
   });
+  
 $('#sidebar .nav>li').click(function(e){
     $('.highlight').removeClass('highlight');
 $(this).addClass('highlight');
@@ -20,7 +85,7 @@ $('.overlayLayer').click(function(e) {
 $(".overlayLayer").click(function () {   
 	//var inputs = dojo.query(".overlayLayer");
 	var inputs = dojo.query(".overlayLayer");
-	var visible = [-1]; //street and city labels
+	var visible = [3,4,11]; //street and city labels
 	for (var i = 0, il = inputs.length; i < il; i++) {
 		var layerLabel = $(inputs[i]).text();  //To select labels to check, we must nest <label tag> in htm. and somehow select that nested label
 		//console.log('layerLabel: ',layerLabel);
@@ -34,7 +99,7 @@ $(".overlayLayer").click(function () {
 		}
 		else {
 			$(layerCheckState).removeClass("active");
-			overlayLayer.setVisibleLayers([-1])
+			overlayLayer.setVisibleLayers([3,4,11])
 			//var elem = $('li.current_sub').prevAll("checked:first");
 			//elem.removeClass("checked");
 			
